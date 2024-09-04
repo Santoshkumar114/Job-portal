3f0abddbd2af7f8e27a2598cd73ee63519313ee5
LINUS2525 <sunil7171ghy@gmail.com>
2024-09-04 22:31:07 +0530

    Update Home.jsx

--- ConnectX-client/src/Pages/Home.jsx
+++ ConnectX-client/src/Pages/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
 
   useEffect(() => {
     setIsLoading(true);
-    fetch("https://backend-gs1h.onrender.com/all-jobs")
+    fetch("https://backend-61ny.onrender.com/all-jobs")
       .then((res) => res.json())
       .then((data) => {
         //console.log(data)
